78f1f7765740ba3b1989233134ed4cb8ca967639
lmogil <lmogil@uchicago.edu>
2025-11-24 11:40:53 -0600

    fix test

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import unittest
 
 from click.testing import CliRunner
@@ -12,8 +13,18 @@ class ThisTestCase(unittest.TestCase):
         self.runner = CliRunner()
 
     def test_pass(self):
-        result = self.runner.invoke(MOD.main)
+#        result = self.runner.invoke(MOD.main)
+#        result = self.runner.invoke(MOD.main, ["--sqlite_path", "/tmp/fake.db"])
+#        self.assertEqual(result.exit_code, 0)
+
+        dummy_db = "/tmp/fake.db"
+        with open(dummy_db, "w") as f:
+            f.write("")  # empty file
+
+        result = self.runner.invoke(MOD.main, ["--sqlite_path", dummy_db])
         self.assertEqual(result.exit_code, 0)
+        # Clean up
+        os.remove(dummy_db)
 
 
 # __END__
