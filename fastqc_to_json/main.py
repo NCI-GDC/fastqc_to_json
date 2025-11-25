6e55f97e63d7ba4afce16ddbcf3c0dff04f7cbce
lmogil <lmogil@uchicago.edu>
2025-11-25 12:06:16 -0600

    fix

--- fastqc_to_json/main.py
+++ fastqc_to_json/main.py
@@ -72,9 +72,10 @@ def main(sqlite_path: str) -> int:
         return 0
 
     # Handle non-empty SQLite file
-    cmd = ["sqlite3", sqlite_path, "select * from fastqc_data_Basic_Statistics;"]
+    #    cmd = ["sqlite3", sqlite_path, "select * from fastqc_data_Basic_Statistics;"]
+    shell_cmd = f'sqlite3 "{sqlite_path}" "select * from fastqc_data_Basic_Statistics;"'
     #   shell_cmd = " ".join(cmd)
-    output = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    output = subprocess.check_output(shell_cmd, shell=True).decode("utf-8")
     output_split = output.split("\n")
     db_to_json(output_split)
 
