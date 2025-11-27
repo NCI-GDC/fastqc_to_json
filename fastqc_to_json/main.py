d88c206d4228176c663d1a84f80280207aaefb36
lmogil <lmogil@uchicago.edu>
2025-11-26 22:55:56 -0600

    fix

--- fastqc_to_json/main.py
+++ fastqc_to_json/main.py
@@ -4,42 +4,39 @@ import argparse
 import json
 import os
 import subprocess
-from typing import Dict, List, Optional, Union
+from typing import Any, Dict, List
 
 
-def db_to_json(result: List[str]) -> None:
-    data: Dict[str, Dict[str, Union[int, str]]] = dict()
-    filename: Optional[str] = None
-
+def db_to_json(result: List) -> Dict[str, Any]:
+    data: Dict[str, Any] = dict()
     for line in result:
         if line == "":
             continue
-
-        parts = line.strip().split("|")
-        if len(parts) < 5:
-            continue
-
-        key = parts[3]
-        value = parts[4]
-
+        line_split = line.strip().split("|")
+        key = line_split[3]
+        value = line_split[4]
         if key == "Filename":
             filename = value
             data[filename] = dict()
-        elif filename is not None:
-            if key in ("File type", "Encoding"):
-                data[filename][key] = value
-            elif key in ("Total Sequences", "Sequences flagged as poor quality", "%GC"):
-                data[filename][key] = int(value)
-            elif key == "Sequence length":
-                if "-" in value:
-                    nums = [int(x) for x in value.split("-")]
-                    value_int = max(nums)
-                else:
-                    value_int = int(value)
-                data[filename][key] = value_int
-
+        elif key == "File type":
+            data[filename][key] = value
+        elif key == "Encoding":
+            data[filename][key] = value
+        elif key == "Total Sequences":
+            data[filename][key] = int(value)
+        elif key == "Sequences flagged as poor quality":
+            data[filename][key] = int(value)
+        elif key == "Sequence length":
+            if ("-") in value:
+                value_split = value.split("-")
+                value_int = [int(x) for x in value_split]
+                value = max(value_int)
+            data[filename][key] = int(value)
+        elif key == "%GC":
+            data[filename][key] = int(value)
     with open("fastqc.json", "w") as fp:
         json.dump(data, fp)
+    return data
 
 
 def main() -> None:
